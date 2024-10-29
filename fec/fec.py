062ecb35764d790212c03ae0877be3054aaa436d
Kasparas Karlauskas <kasparas.karlauskas@gmail.com>
2024-10-30 01:16:38 +0900

    Add coordinate offsetting to avoid corrupt results due to overflow

--- fec/fec.py
+++ fec/fec.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from fec._fec import _FEC
 
-def FEC(xyz, min_component_size, tolerance, max_n):
+def FEC(xyz, min_component_size, tolerance, max_n, offset=True):
     """
     Fast Euclidean Clustering of a 3D point cloud.
 
@@ -11,6 +11,7 @@ def FEC(xyz, min_component_size, tolerance, max_n):
         min_component_size (int): ...
         tolerance (float): sphere radius defining a point's neighborhood
         max_n (int): maximum number of neighborhood points returned in nearest neighbor query
+        offset (bool): apply offset so the lowest coordinate in each axis is 0 to avoid overflow
 
     Returns:
         list[int]: cluster index for every point 
@@ -18,4 +19,8 @@ def FEC(xyz, min_component_size, tolerance, max_n):
     Reference:
         https://arxiv.org/abs/2208.07678
     """
-    return _FEC(list(xyz.flatten()), min_component_size, tolerance, max_n)
+    if not offset:
+        flat_xyz = xyz.flatten()
+    else:
+        flat_xyz = (xyz - np.min(xyz, axis=0)).flatten()
+    return _FEC(flat_xyz, min_component_size, tolerance, max_n)
